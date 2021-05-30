82f07c698e87cd3bfc0b19ff79a319b93380b79f
Marron Akanishi <marron.general@gmail.com>
2021-05-30 12:53:27 +0900

    PostFormの送信

--- free_method/main.go
+++ free_method/main.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
+	"strings"
 )
 
 func main() {
+	values := url.Values{"test": {"value"}}
+	reader := strings.NewReader(values.Encode())
+
 	client := &http.Client{}
-	request, err := http.NewRequest("DELETE", "http://localhost:18888", nil)
+	request, err := http.NewRequest("DELETE", "http://localhost:18888", reader)
 	if err != nil {
 		panic(err)
 	}
