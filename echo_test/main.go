75ba2e5aff24992a30a84142fe3789e93998b4f1
Marron Akanishi <marron.general@gmail.com>
2021-05-30 12:30:13 +0900

    クッキーの送受信

--- echo_test/main.go
+++ echo_test/main.go
@@ -19,9 +19,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
+func handlerCookie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	if _, ok := r.Header["Cookie"]; ok {
+		fmt.Fprintf(w, "<html><body>2回目以降</body></html>\n")
+	} else {
+		fmt.Fprintf(w, "<html><body>初訪問</body></html>\n")
+	}
+}
+
 func main() {
 	var httpServer http.Server
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/cookie", handlerCookie)
 	log.Println("start http listening :18888")
 	httpServer.Addr = ":18888"
 	log.Println(httpServer.ListenAndServe())
